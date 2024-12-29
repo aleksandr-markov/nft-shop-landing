96e580447cbf249876d73433bfba5430b2180b71
Oleksandr Markov <markovvvv.aleksandr@gmail.com>
2024-12-29 16:11:48 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        primary: "#D6EF0E",
+      },
+    },
+  },
+  plugins: [],
+};
